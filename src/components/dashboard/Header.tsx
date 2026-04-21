0e5621327b332b26c50f2491f17c77ad7b2b5fef
Mohammad Samir <mohammadsamir2424@gmail.com>
2026-04-21 20:21:10 +0000

    Try fixing this error: `Runtime TypeError: Failed to set an indexed prop

--- src/components/dashboard/Header.tsx
+++ src/components/dashboard/Header.tsx
@@ -183,7 +183,7 @@ export default function Header({
             >
               <motion.div
                 initial={{ opacity: 0, y: 20, scale: 0.95 }}
-                animate={{ 1: 1, y: 0, scale: 1 }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
                 exit={{ opacity: 0, y: 20, scale: 0.95 }}
                 transition={panelSpring}
               >
